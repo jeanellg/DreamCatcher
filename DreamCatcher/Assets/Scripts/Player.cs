c63efdbe456e09944f4d2bdffa6dadd94da1db6b
Daniel Holmes <daniel.holmes11@gmail.com>
2015-10-26 18:41:43 -0700

    Added multiple (double, triple, etc) jumps

--- DreamCatcher/Assets/Scripts/Player.cs
+++ DreamCatcher/Assets/Scripts/Player.cs
@@ -6,17 +6,21 @@ public class Player : MonoBehaviour {
     public float speed;
     public float jumpForce;
     public string groundTag;
+    public int numberOfHops;
 
     private KeyCode left = KeyCode.A;
     private KeyCode right = KeyCode.D;
     private KeyCode jump = KeyCode.Space;
-    private Vector2 jumpVector;
     private bool grounded = false;
+    private int hopsRemaining = 0;
+    private Rigidbody2D rb;
+    private Vector2 jumpVector;
 
 	// Use this for initialization
 	void Start ()
     {
         jumpVector = new Vector2(0, jumpForce);
+        rb = this.GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
@@ -37,16 +41,23 @@ public class Player : MonoBehaviour {
     {
         if (Input.GetKey(jump) && grounded)
         {
-            this.transform.GetComponent<Rigidbody2D>().AddForce(jumpVector);
+            rb.AddForce(jumpVector);
             grounded = false;
+        }
+        else if (!grounded && Input.GetKeyDown(jump) && hopsRemaining > 0)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, 0);
+            rb.AddForce(jumpVector);
+            hopsRemaining--;
         }
     }
 
-    void OnCollisionStay2D(Collision2D col)
+    void OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.tag == groundTag)
         {
             grounded = true;
+            hopsRemaining = numberOfHops;
         }
     }
 }
