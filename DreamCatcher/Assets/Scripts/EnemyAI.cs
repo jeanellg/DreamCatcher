f0b4c19de26887e5d77c9b3aeddba222062ff94a
woo00154 <wshwang@uci.edu>
2015-11-19 18:39:29 -0800

    Revert "small update on enemy AI"
    This reverts commit 8a9679f8a4a00f1ec9e0decf079bbee29272e64f.

--- DreamCatcher/Assets/Scripts/EnemyAI.cs
+++ DreamCatcher/Assets/Scripts/EnemyAI.cs
@@ -3,46 +3,46 @@ using System.Collections;
 
 public class EnemyAI : MonoBehaviour {
     //I want to make this into an abstract class
-    public const float view_distance = 5.0f;
-    public const float patrol_speed = 3.0f;
-    public const float alert_speed = 6.0f;
+    public float viewDistance;
+    public float defaultSpeed;
 
-    private string[] states = new string[3] {"idle","patrol","alert"};
-    private string current_state;
-    private float current_speed;
-    private Vector2 direction;
+
     private GameObject enemy;
-    
+    private Vector2 direction;
 
     // Use this for initialization
     void Start () {
+        viewDistance = 5.0f;
+        defaultSpeed = 3.0f;
 
-        
         //set default direction to right
         direction = Vector2.right;
         enemy = transform.gameObject;
-        current_state = states[1];
+        
     }
 
-    void updateState ()
+    void FixedUpdate()
     {
+        Vector3 centerPoint = enemy.transform.position;
+        RaycastHit2D[] detect = Physics2D.RaycastAll(centerPoint, direction, viewDistance);
 
-    }
+        for (int i = 0; i < detect.Length; i++)
+        {
+            string s = detect[i].collider.tag;
 
-    void updateSpeed ()
-    {
-        if (current_state == states[0])
-            current_speed = 0;
-        else if (current_state == states[1])
-            current_speed = patrol_speed;
-        else if (current_state == states[2])
-            current_speed = alert_speed;
-        else
-            print("State: " + current_state + " is undefined");
-    }
+            if (s == "player")
+            {
+                //do something
 
-    void updateDirection (Vector3 centerPoint)
-    {
+            }
+            else
+            {
+                //do other things
+            }
+
+
+        }
+
         Vector2 lookDown = new Vector2(direction.x, -1);
         RaycastHit2D[] fall = Physics2D.RaycastAll(centerPoint, lookDown, 2.0f);
         bool change_direction = true;
@@ -51,52 +51,13 @@ public class EnemyAI : MonoBehaviour {
             string f = fall[i].collider.tag;
             if (f == "ground")
                 change_direction = false;
+
         }
-
-    
-
         if (change_direction)
             //direction = Vector2.left;
             direction = -direction;
-    }
 
-    void forwardDetection (Vector3 centerPoint)
-    {
-        RaycastHit2D[] detect = Physics2D.RaycastAll(centerPoint, direction, view_distance);
-
-        for (int i = 0; i < detect.Length; i++)
-        {
-            string s = detect[i].collider.tag;
-
-            if (s == "player")
-            {
-                //ignore regular patrol pattern
-
-            }
-            else if (s == "wall")
-            {
-                //do other things
-            }
-
-
-        }
-    }
-
-    void FixedUpdate()
-    {
-        Vector3 centerPoint = enemy.transform.position;
-        updateState();
-        updateSpeed();
-        
-
-
-        updateDirection(centerPoint);
-        forwardDetection(centerPoint);
-
-
-        
-
-        enemy.GetComponent<Rigidbody2D>().velocity = new Vector2((direction.x * current_speed), enemy.GetComponent<Rigidbody2D>().velocity.y);
+        enemy.GetComponent<Rigidbody2D>().velocity = new Vector2((direction.x * defaultSpeed), enemy.GetComponent<Rigidbody2D>().velocity.y);
     }
 
 }
