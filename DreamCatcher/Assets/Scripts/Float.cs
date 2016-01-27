82cc920ba823f6ed6a259649a1adfd6b3f8047a5
Jacob Mikesell <jacob.mikesell96@gmail.com>
2016-01-27 14:07:32 -0800

    Rudimentary flashlight and float scripts, still buggy, pushing early to move them to my other computer

---
+++ DreamCatcher/Assets/Scripts/Float.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Float : MonoBehaviour {
+
+	public GameObject player;
+	public KeyCode On = KeyCode.N;
+	public float rate = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(On)){
+			player.transform.position += new Vector3(0,rate*Time.deltaTime,0);
+		}
+	}
+}
