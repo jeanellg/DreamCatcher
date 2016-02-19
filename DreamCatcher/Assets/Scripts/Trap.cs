64fcf4f4446f01acc4d03c649eb461cf534a59ee
Jacob Mikesell <Jacob.mikesell96@gmail.com>
2016-02-19 14:59:00 -0800

    Created several WIP prefabs for level implimentation

---
+++ DreamCatcher/Assets/Scripts/Trap.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trap : MonoBehaviour {
+	public bool isActive;
+	public Rigidbody2D dart;
+	public int numberOfDarts =1;
+	public Lever lever;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if (isActive && numberOfDarts>0 && lever.getState() ==1){
+		Rigidbody2D dartInstance = Instantiate (dart, transform.position, Quaternion.identity) as Rigidbody2D;
+		dartInstance.velocity = new Vector2(-10,0);
+		numberOfDarts--;
+	}
+	}
+}
