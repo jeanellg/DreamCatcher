9c6976cec6f2e30aee6fb91511fd52e4b20a3a4e
Jacob Mikesell <Jacob.mikesell96@gmail.com>
2016-02-26 16:16:46 -0800

    Updated the tutorial level and float script.

--- DreamCatcher/Assets/Scripts/Float.cs
+++ DreamCatcher/Assets/Scripts/Float.cs
@@ -3,12 +3,11 @@ using System.Collections;
 
 public class Float : MonoBehaviour {
 
-	public GameObject player;
 	public Rigidbody2D rb;
 	public KeyCode On = KeyCode.N;
 	public string floatTag;
 	public bool canFloat;
-	public float rate = 2.0f;
+	public float rate = 200f;
 
 	// Use this for initialization
 	void Start () {
@@ -19,17 +18,23 @@ public class Float : MonoBehaviour {
 	void Update () {
 		if (Input.GetKey(On) && canFloat){
 			rb.gravityScale = 0f;
-			player.transform.position += new Vector3(0,rate*Time.deltaTime,0);
+			rb.velocity = new Vector2(rb.velocity.x, 0);
+			rb.AddForce(new Vector2(0, rate));
 		}
 		else{
 			rb.gravityScale = 1f;
 		}
 	}
-	 void OnCollisionEnter2D(Collision2D col)
+	 void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.tag == floatTag)
         {
             canFloat = true;
         }
+    }
+    void OnTriggerExit2D(Collider2D col){
+    	if (col.gameObject.tag == floatTag){
+    	canFloat = false;
+    	}
     }
 }
